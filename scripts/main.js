5bf0c5dbc4abfe4214c4151fde07154fc2cb4bb9
Greck2908 <rallistab-82@bk.ru>
2021-04-09 21:57:49 +0600

    Добавление слайдов + скрипт

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.channel-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+
+});
